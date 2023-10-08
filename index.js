e4238fe345e16821b6733e212d7c873454e846c2
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-10-09 00:53:05 +0300

    complETed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+
+const mainElement = document.querySelector('main');
+mainElement.remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+
+document.body.appendChild(newHeader);
